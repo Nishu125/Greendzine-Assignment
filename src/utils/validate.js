31abe428c0dafef30c7459bbb18de1fe92fa9dda
Greendzine <nishanthsl512@gmail.com>
2024-03-04 21:09:07 +0530

    second commit

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,10 +1,11 @@
 export const checkValidateData=(email, password)=>{
     const isEmailValid= /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
     // const isPasswordValid= /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-    const isPasswordValid = /.{1,}/.test(password);
+    // const isPasswordValid = /.{1,}/.test(password);
+    const isPasswordValid=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
     if(!isEmailValid) return "Email id is not valid";
-    if(!isPasswordValid) return "Password is not valid";
+    if(!isPasswordValid) return "The password should contain at least 8 characters including one uppercase and lowercase letter, one number, and one special character";
 
 
     return null;
