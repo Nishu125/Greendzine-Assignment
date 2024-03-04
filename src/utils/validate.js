b8337a018056b4c7f4069a305bad14d216184609
Greendzine <nishanthsl512@gmail.com>
2024-03-04 21:10:25 +0530

    second commit

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,7 +1,5 @@
 export const checkValidateData=(email, password)=>{
     const isEmailValid= /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
-    // const isPasswordValid= /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-    // const isPasswordValid = /.{1,}/.test(password);
     const isPasswordValid=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
     if(!isEmailValid) return "Email id is not valid";
